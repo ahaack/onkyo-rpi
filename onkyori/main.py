34ae19d36aac38f48815652db65baa84c1902109
Andreas Haack <andreashaack@hotmail.com>
2019-08-13 23:27:22 +0200

    Integrate argparse for main.py and scan.py

--- onkyori/main.py
+++ onkyori/main.py
@@ -1,19 +1,33 @@
+import argparse
 import sys
 import time
 import pigpio
 from command import OnkyoCommand
 
-GPIO = 4
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='A tool to control onkyo devices over Remote Interactive (RI) interface.')
+    parser.add_argument('message', metavar='message', type=lambda x: int(x, 0), nargs=1,
+                        help='message (0x000-0xFFF or 0-4095) to send to your onkyo device')
+    parser.add_argument('--gpio', type=int, nargs='?', default=25,
+                        help="GPIO port of your pi you want to use - default: 25")
+
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
 
+    args = parse_args()
+
     pi = pigpio.pi()
 
-    args = len(sys.argv)
+    message = args.message[0]
+    print('send message {:012b} ({:04d}) via GPIO {:d}'
+          .format(message, message, args.gpio))
 
-    if args > 1:
-        command = OnkyoCommand(pi, gpio=GPIO)
-        command.send(int(sys.argv[1]))
-        time.sleep(1)
+    command = OnkyoCommand(pi, gpio=int(args.gpio))
+    command.send(int(args.message[0]))
+    time.sleep(1)
 
     pi.stop()
--- onkyori/scan.py
+++ onkyori/scan.py
@@ -1,16 +1,41 @@
 import time
 import pigpio
+import argparse
 from command import OnkyoCommand
 
 GPIO = 25
 
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='A tool to scan all messages for your onkyo Remote Interactive (RI) interface.')
+    parser.add_argument('--lower', type=lambda x: int(x, 0), nargs='?', default=0,
+                        help='lower boundary (0x000-0xFFF or 0-4095) - default: 0')
+    parser.add_argument('--upper', type=lambda x: int(x, 0), nargs='?', default=4095,
+                        help='upper boundary (0x000-0xFFF or 0-4095) - default: 4095')
+    parser.add_argument('--gpio', type=int, nargs='?', default=25,
+                        help="GPIO port of your pi you want to use - default: 25")
+    parser.add_argument('--delay', type=int, nargs='?', default=0,
+                        help="sleep delay in seconds between commands - default: 0")
+
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
+    args = parse_args()
 
     pi = pigpio.pi()
 
-    for x in range(0, 4095):
-        print("command: " + str(x))
-        command = OnkyoCommand(pi, gpio=GPIO)
+    lower = int(args.lower)
+    upper = int(args.upper)
+
+    print('scan from {:012b} ({:04d}) to {:012b} ({:04d})'
+          .format(lower, lower, upper, upper))
+
+    for x in range(lower, upper):
+        print("command: {:012b} ({:04d})".format(x, x))
+        command = OnkyoCommand(pi, gpio=int(args.gpio))
         command.send(x)
+        time.sleep(int(args.delay))
 
     pi.stop()
